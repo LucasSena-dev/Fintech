ae2cd9178dbf63fbeeae5bc03c14796c9268c51b
Lucas da Cruz Sena <cruzlucas.sena@gmail.com>
2024-06-17 21:35:47 -0300

    update
    update scripts

--- js/scriptSignUp.js
+++ js/scriptSignUp.js
@@ -29,11 +29,5 @@ document.getElementById('toggleConfirmPassword').addEventListener('click', funct
     this.classList.toggle('fa-eye-slash');
 });
 
-document.getElementById('togglePasswordButton').addEventListener('click', function() {
-    const passwordInput = document.getElementById('password');
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
 
-    this.classList.toggle('fa-eye');
-    this.classList.toggle('fa-eye-slash');
-});
+
